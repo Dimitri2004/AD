2e1f6bd7230fe089977f80956fa94343c5d6cac8
dam <daapariciopereira2@danielcastelao.org>
2025-09-12 13:35:02 +0200

    Primera practica 0.2

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
-        eDirectoerio("/home/dam/Escritorio/AD");
-        eFichero("/home/dam/Escritorio/AD\n");
-        crearDirectorio("/home/dam/Escritorio/AD\n");
+    public static void main(String[] args) throws IOException {
+        eDirectoerio("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n");
+        eFichero("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n");
+        crearDirectorio("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n");
+        crearFichero("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        crearFichero("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Desventaja.txt");
+        mEscribir("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        mLectura("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        modoAcceso("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        //mBorrar("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        //mBorrarDire("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n", "Ventaja.txt");
+        mArchivos("/home/dam/Escritorio/Kotlin/Directorios/Fichero\n");
+
     }
 
     /**
      * @param cadena
      * @return
      */
-    public static Boolean eDirectoerio(String cadena){
-        File file=new File(cadena);
+    public static Boolean eDirectoerio(String cadena) {
+        File file = new File(cadena);
 
         if (file.isDirectory()) {
             return true;
-        }else {
+        } else {
             return false;
         }
-        }
+    }
 
     /**
      *
      * @param cadena
      * @return
      */
-    public static boolean eFichero(String cadena){
-        File carpeta=new File(cadena);
-        if (carpeta.isFile()){
+    public static boolean eFichero(String cadena) {
+        File carpeta = new File(cadena);
+        if (carpeta.isFile()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -42,56 +52,91 @@ public class Main {
      *
      * @param ruta
      */
-    public static void crearDirectorio(String ruta){
-        File archivo=new File(ruta);
-        boolean crea=archivo.mkdir();
-        if (crea){
+    public static void crearDirectorio(String ruta) {
+        File archivo = new File(ruta);
+        boolean crea = archivo.mkdir();
+        if (crea) {
             System.out.println("Creado existosamente");
-        }else{
+        } else {
             System.out.println("No se pudo generar");
         }
     }
 
-    public static void crearFichero(String nomeDire,String nomeFile ) throws IOException {
-        File archivo=new File(nomeDire,nomeFile);
-        if(archivo.createNewFile()){
+    public static void crearFichero(String nomeDire, String nomeFile) throws IOException {
+        File archivo = new File(nomeDire, nomeFile);
+        if (archivo.createNewFile()) {
             System.out.println("Creado exitosamente");
-        }else {
+        } else {
             System.out.println("Fallo en crear archivo");
         }
 
     }
 
-    public static void modoAcceso(String nomeDire,String nomeFile ) throws IOException {
-        File archivo=new File(nomeDire,nomeFile);
-        if(archivo.canWrite()){
+    public static void modoAcceso(String nomeDire, String nomeFile) throws IOException {
+        File archivo = new File(nomeDire, nomeFile);
+        if (archivo.canWrite()) {
             System.out.println("Es posible escribir");
-        }else {
+        } else {
             System.out.println("No es posible escribir");
         }
-        if (archivo.canRead()){
-            System.out.println("Esrte archivo se puede ver");
-        }else{
+        if (archivo.canRead()) {
+            System.out.println("Este archivo se puede ver");
+        } else {
             System.out.println("Este archivo no se puede ver");
         }
     }
-    public static void Tamaño(String nomeDire,String nomeFile ) {
+
+    public static void Tamaño(String nomeDire, String nomeFile) {
         long tamañoArchivo = 0;
-        File archivo=new File(nomeDire,nomeFile);
+        File archivo = new File(nomeDire, nomeFile);
         tamañoArchivo = archivo.length();
 
-        System.out.println("Ocupara : "+tamañoArchivo+" bites");
+        System.out.println("Ocupara : " + tamañoArchivo + " bites");
     }
-    public static void mLectura(String nomeDire,String nomeFile){
-        File archivo = new File(nomeDire,nomeFile);
-        boolean correcto = archivo.setReadOnly();
-        if (correcto){
+
+    public static void mLectura(String nomeDire, String nomeFile)  {
+        File archivo = new File(nomeDire, nomeFile);
+        boolean correcto = archivo.setReadable(true);
+        if (correcto) {
             System.out.println("Se puede leer");
-        }else{
+        } else {
             System.out.println("No se puede leer");
         }
     }
 
+    public static void mEscribir(String nomeDire, String nomeFile) throws IOException {
+        File archivo = new File(nomeDire, nomeFile);
+        boolean escribir = archivo.setWritable(true);
+        if (escribir) {
+            System.out.println("se puede escribiR");
+        }
+    }
 
 
+    public static void mBorrar(String nomeDire, String nomeFile){
+        File archivo= new File(nomeDire,nomeFile);
+        boolean borrado=archivo.delete();
+        if (borrado){
+            System.out.println("Borrado");
+        }else{
+            System.out.println("No existe el archivo");
+        }
+    }
+    public static void mBorrarDire(String nomeDire, String nomeFile) {
+        File archivo= new File(nomeDire);
+        boolean borrado=archivo.delete();
+        if (borrado){
+            System.out.println("Borrado");
+        }else{
+            System.out.println("No existe el archivo");
+        }
+    }
+    public static void  mArchivos(String nomeDire) {
+        File archivo=new File(nomeDire);
+
+        for (String s : Objects.requireNonNull(archivo.list())) {
+            System.out.println(s);
+        }
+    }
 }
+
