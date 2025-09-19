e38978d9675eaaa8236cdcd081562dbfc2ee1802
dam <daapariciopereira2@danielcastelao.org>
2025-09-19 14:43:09 +0200

    Primera practica 3 apartado 3 DataInput/Output de ficheros 0.1

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
-
-
 
 
 public class Tarea3 {
@@ -12,9 +9,9 @@ public class Tarea3 {
         int repeticiones=4;
 
         FileInputStream n1=new FileInputStream(r2);
-        DataInputStream n2=new DataInputStream(n1);
-        FileOutputStream p=new FileOutputStream(r2);
-        DataOutputStream b=new DataOutputStream(p);
+        DataInputStream n2=new DataInputStream(n1);//recorre la cadena del input y recorre la misma
+        FileOutputStream p=new FileOutputStream(r2);//recoge datos del input del fichero hacia el nuevo
+        DataOutputStream b=new DataOutputStream(p);//recoge la cadena del input y escribe en un nuevo fichero
         int i=0;
         int calculo= 0;
        while (i<repeticiones){
@@ -24,7 +21,7 @@ public class Tarea3 {
             if (i==0) {
                 calculo = b.size() - texto.length();
             }
-           System.out.println("Tamaño:"+b.size());
+           System.out.println("Tamaño:"+b.size());//Añae espacio en base al tamaño del texto a añadir
            i++;
        }
        int tamañoFinal=b.size();
@@ -36,13 +33,10 @@ public class Tarea3 {
 
             int restante=tamañoFinal-tamaño-calculo;
             tamañoFinal=restante;
-            System.out.println("Te queda: "+restante);
+            System.out.println("Te queda: "+restante);//Da un numero de espacio restante despues de que el texto ocupara un trozo del total
             j++;
         }
-
     }
-
-
 
     public static void  LecturaBuffer() throws IOException {
         String r1="/home/dam/Escritorio/Kotlin/Directorios/src/Imagen1.jpeg";
@@ -51,10 +45,10 @@ public class Tarea3 {
         FileOutputStream n2=new FileOutputStream(r2);
         BufferedInputStream b=new BufferedInputStream(n);
         BufferedOutputStream a=new BufferedOutputStream(n2);
-        a.write(b.readAllBytes());//Selecciona todos los bytes que tenga y los copia
+        a.write(b.readAllBytes());//Selecciona todos los bytes que tenga y los copia haciendo una copia exacta en menos tiempo
         a.close();
     }
-    public static void Ficheros() throws IOException {
+    public static void FicherosCopiados() throws IOException {
         String ruta="/home/dam/Escritorio/Kotlin/Directorios/src/Texto.txt";
         String ruta2="/home/dam/Escritorio/Kotlin/Directorios/src/Texto2.txt";
         FileInputStream n=new FileInputStream(ruta);
@@ -71,7 +65,7 @@ public class Tarea3 {
             }
         }
         FileOutputStream h=new FileOutputStream(ruta2,true);
-        h.write(b);
+        h.write(b);//Recrea lo copiado con FileInputStream de un fichero, tranformados en bytes ,y reescritos en otro con el FileOutput en otro
         h.close();
     }
     public static void Copia() throws IOException {
@@ -91,7 +85,7 @@ public class Tarea3 {
             }
         }
         FileOutputStream h=new FileOutputStream(r2,true);
-        h.write(b);
+        h.write(b);//Copia una imagen y recrea la misma pero con espacio duplicado
         h.close();
     }
 
@@ -100,6 +94,9 @@ public class Tarea3 {
 
     public static void main(String[] args) throws IOException {
         LecturaData();
+        FicherosCopiados();
+        Copia();
+        LecturaBuffer();
     }
 
 
