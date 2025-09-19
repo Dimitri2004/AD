26cd287dc6f8b327f4de6354ae61159d3accfed1
dam <daapariciopereira2@danielcastelao.org>
2025-09-19 12:48:05 +0200

    Primera practica 3 apartado 2 BufferInput y outputReader

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -1,16 +1,18 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 
 public class Tarea3 {
-    public static void main(String[] args) throws IOException {
-        Ficheros();
 
-
-
+    public static void  Lectura() throws IOException {
+        String r1="/home/dam/Escritorio/Kotlin/Directorios/src/Imagen1.jpeg";
+        String r2="/home/dam/Escritorio/Kotlin/Directorios/src/Imagen2.jpeg";
+        FileInputStream n=new FileInputStream(r1);
+        FileOutputStream n2=new FileOutputStream(r2);
+        BufferedInputStream b=new BufferedInputStream(n);
+        BufferedOutputStream a=new BufferedOutputStream(n2);
+        a.write(b.readAllBytes());//Selecciona todos los bytes que tenga y los copia
+        a.close();
     }
-
     public static void Ficheros() throws IOException {
         String ruta="/home/dam/Escritorio/Kotlin/Directorios/src/Texto.txt";
         String ruta2="/home/dam/Escritorio/Kotlin/Directorios/src/Texto2.txt";
@@ -31,6 +33,34 @@ public class Tarea3 {
         h.write(b);
         h.close();
     }
+    public static void Copia() throws IOException {
+        String r1="/home/dam/Escritorio/Kotlin/Directorios/src/Imagen1.jpeg";
+        String r2="/home/dam/Escritorio/Kotlin/Directorios/src/Imagen2.jpeg";
+        FileInputStream n=new FileInputStream(r1);
+        byte[] b = new byte[n.available()];
+        int i=0;
+        while (true) {
+            int m=n.read();
+            if (m==-1){
+                break;
+            }else {
+                byte bites = (byte) m;
+                b[i] = bites;
+                i++;
+            }
+        }
+        FileOutputStream h=new FileOutputStream(r2,true);
+        h.write(b);
+        h.close();
+    }
+
+
+    /** Parte2 **/
+
+    public static void main(String[] args) throws IOException {
+        Lectura();
+    }
+
 
 
 }
